10543d34506bd6786b291aaae321ea20f53a8d0c
Ido Ben-Hur <idoybh2@gmail.com>
2024-12-17 21:19:08 +0200

    Launcher3: Make sure we start the app again after killing it
    Change-Id: I1358e20ed6f59bdc311873efa5664a90959de0ea

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -228,7 +228,7 @@ public class LauncherAppState implements SafeCloseable {
                     MODEL_EXECUTOR.execute(() -> mIconCache.clearDb());
                     mModel.forceReload();
                     if ((flags & CHANGE_OVERLAYS) != 0) {
-                        Utilities.restart();
+                        Utilities.restart(mContext);
                     }
                 }
             }
@@ -266,7 +266,7 @@ public class LauncherAppState implements SafeCloseable {
         // so we force a restart now if needed without waiting for home button press
         if (mNeedsRestart) {
             Toast.makeText(mContext, R.string.restarting_launcher_changes, Toast.LENGTH_SHORT).show();
-            Utilities.restart();
+            Utilities.restart(mContext);
         }
     }
 
